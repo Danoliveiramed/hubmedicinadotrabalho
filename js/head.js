25c7e48a07b60baae2d0cb3a465be565962c17c1
Daniel <dan.oliveira.med@gmail.com>
2026-04-19 16:40:24 -0300

    favicon

--- js/head.js
+++ js/head.js
@@ -7,6 +7,6 @@ function carregarHead(titulo = "") {
 
   const favicon = document.createElement("link");
   favicon.rel = "icon";
-  favicon.href = "/assets/favicon.ico";
+  favicon.href = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🩺</text></svg>"; //favicon.href = "/assets/favicon.ico";
   document.head.appendChild(favicon);
 }
